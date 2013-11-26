c2857b95a5e2cfb8273e521faca4538fc8880a77
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-11-26 07:26:18 +0100

    Correctly recognize validation errors

--- check_failed_xml.sh
+++ check_failed_xml.sh
@@ -5,5 +5,5 @@ ALTO=ALTO
 MODSXML=MODS
 
 echo "$1"/ALTO/* | sed 's/xml /xml\n/g' > altos
-grep -i ikke $LOG | grep -i alto | cut -d' ' -f1 | sed 's/\.xml//' | while read I; do egrep $I altos; done | while read J; do echo tjekker ALTO: $J; xmllint --noout --schema alto-1-4-fixed.xsd "$J" 2>&1; echo \#\#; echo \#\#; done
-grep -i ikke $LOG | grep -i mods | cut -d' ' -f1 | sed 's/-MODS\.xml//' | while read I; do grep $I altos; done | sed "s/\/$ALTO\//\/$MODSXML\//" | sed 's/\.xml/-MODS\.xml/' | while read J; do echo tjekker MODS: $J; xmllint --noout --schema mods-3-1.xsd $J 2>&1; echo \#\#; echo \#\#; done
+grep -i 'validerer ikke' $LOG | grep -i alto | cut -d' ' -f1 | sed 's/\.xml//' | while read I; do egrep $I altos; done | while read J; do echo tjekker ALTO: $J; xmllint --noout --schema alto-1-4-fixed.xsd "$J" 2>&1; echo \#\#; echo \#\#; done
+grep -i 'validerer ikke' $LOG | grep -i mods | cut -d' ' -f1 | sed 's/-MODS\.xml//' | while read I; do grep $I altos; done | sed "s/\/$ALTO\//\/$MODSXML\//" | sed 's/\.xml/-MODS\.xml/' | while read J; do echo tjekker MODS: $J; xmllint --noout --schema mods-3-1.xsd $J 2>&1; echo \#\#; echo \#\#; done
--- visfejl.sh
+++ visfejl.sh
@@ -5,7 +5,7 @@ if [ -z $LOG ]; then
 fi
 #echo antal TIF i alt: $(wc -l tifs) \(aktuelt mount\)
 echo antal filer analyseret: $(grep ^[0-9] $LOG | sort -u | wc -l) 
-echo alto-XML-fejl: $(./fejl.sh $LOG | egrep -i 'alto.*ikke' | wc -l) \(kan have falske positiver\)
+echo alto-XML-fejl: $(./fejl.sh $LOG | egrep -i 'alto.*validerer ikke' | wc -l) \(kan have falske positiver\)
 echo mods-XML-fejl: $(egrep -i 'mods.*ikke' $LOG | wc -l) \(kan have falske positiver\)
 AF=$(grep -i fejl $LOG | grep -v TIF-tags | wc -l);
 echo antal andre fejl: $AF; if [ $AF -ge 1 ]; then grep -i fejl $LOG | grep -v TIF-tags; fi
