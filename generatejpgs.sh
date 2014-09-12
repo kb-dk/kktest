994c73b30d896735e5fce6e65c8ee00cb0987537
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-09-12 12:51:27 +0200

    Reformat scripts

--- generatejpgs.sh
+++ generatejpgs.sh
@@ -1,1 +1,9 @@
-for DIR in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do IFS=''; find "$DIR"/ -name "*.jpg" -printf %P\\0|while read -d $'\0' -r i; do mkdir -p "$(basename "$DIR")/$(dirname $i)"; convert -units PixelsPerInch "$DIR/$i" -resample 96 "$(basename "$DIR")/$i" ;done; done
+#!/bin/bash
+
+for DIR in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do 
+    IFS=' '
+    find "$DIR"/ -name "*.jpg" -printf %P\\0 | while read -d $'\0' -r FILE; do 
+        mkdir -p "$(basename "$DIR")/$(dirname "$FILE")"
+        convert -units PixelsPerInch "$DIR/$FILE" -resample 96 "$(basename "$DIR")/$FILE"
+    done
+done
--- generatepdfs.sh
+++ generatepdfs.sh
@@ -1,1 +1,9 @@
-for DIR in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do IFS=''; find "$DIR"/ -name "*.pdf" -printf %P\\0|while read -d $'\0' -r i; do mkdir -p "$(basename "$DIR")/$(dirname $i)"; gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor -dPDFOPTIONS=/screen -dDownsampleColorImages=true -dDownsampleGrayImages=true -dDownsampleMonoImages=true -dColorImageResolution=96 -dGrayImageResolution=96 -dGrayImageResolution=96 -sDEVICE=pdfwrite -sOutputFile="$(basename "$DIR")/$i" "$DIR/$i";done; done
+#!/bin/bash
+
+for DIR in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do 
+    IFS=' '
+    find "$DIR"/ -name "*.pdf" -printf %P\\0 | while read -d $'\0' -r FILE; do 
+        mkdir -p "$(basename "$DIR")/$(dirname $FILE)"
+        gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor -dPDFOPTIONS=/screen -dDownsampleColorImages=true -dDownsampleGrayImages=true -dDownsampleMonoImages=true -dColorImageResolution=96 -dGrayImageResolution=96 -dGrayImageResolution=96 -sDEVICE=pdfwrite -sOutputFile="$(basename "$DIR")/$FILE" "$DIR/$FILE"
+    done
+done
