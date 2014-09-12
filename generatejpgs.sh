e6ec9142046d7c42ebab9b7a9a453f2cfd2780f6
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-09-12 12:09:18 +0200

    Add scripts to generate dissemination copies

---
+++ generatejpgs.sh
@@ -0,0 +1,1 @@
+for DIR in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do IFS=''; find "$DIR"/ -name "*.jpg" -printf %P\\0|while read -d $'\0' -r i; do mkdir -p "$(basename "$DIR")/$(dirname $i)"; convert -units PixelsPerInch "$DIR/$i" -resample 96 "$(basename "$DIR")/$i" ;done; done
