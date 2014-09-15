e29867be3f10be4b2ed3edbe1541380b8c6a3618
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-09-15 09:16:01 +0200

    Review followup: also scale mono images; bug in IFS on more than one space; honour file names with spaces; and style issues

--- generatejpgs.sh
+++ generatejpgs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for DIR in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do 
-    IFS=' '
-    find "$DIR"/ -name "*.jpg" -printf %P\\0 | while read -d $'\0' -r FILE; do 
-        mkdir -p "$(basename "$DIR")/$(dirname "$FILE")"
-        convert -units PixelsPerInch "$DIR/$FILE" -resample 96 "$(basename "$DIR")/$FILE"
+for dir in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do 
+    IFS=''
+    find "$dir"/ -name "*.jpg" -printf %P\\0 | while read -d $'\0' -r file; do 
+        mkdir -p "$(basename "$dir")/$(dirname "$file")"
+        convert -units PixelsPerInch "$dir/$file" \
+                -resample 96 \
+                "$(basename "$dir")/$file"
     done
 done
--- generatepdfs.sh
+++ generatepdfs.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-for DIR in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do 
-    IFS=' '
-    find "$DIR"/ -name "*.pdf" -printf %P\\0 | while read -d $'\0' -r FILE; do 
-        mkdir -p "$(basename "$DIR")/$(dirname $FILE)"
-        gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor -dPDFOPTIONS=/screen -dDownsampleColorImages=true -dDownsampleGrayImages=true -dDownsampleMonoImages=true -dColorImageResolution=96 -dGrayImageResolution=96 -dGrayImageResolution=96 -sDEVICE=pdfwrite -sOutputFile="$(basename "$DIR")/$FILE" "$DIR/$FILE"
+for dir in /sbftp-home/scan-dk2/OM/Rettelse\ PH\ 24-01-2014 /sbftp-home/scan-dk2/OM/Rettelse\ PML\ 16-04-2014 /sbftp-home/scan-dk2/OM/Rettelse\ KS\ 06-05-2014; do 
+    IFS=''
+    find "$dir"/ -name "*.pdf" -printf %P\\0 | while read -d $'\0' -r file; do 
+        mkdir -p "$(basename "$dir")/$(dirname "$file")"
+        #http://stackoverflow.com/questions/9497120/how-to-downsample-images-within-pdf-file/9571488#9571488
+        gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor \
+           -dPDFOPTIONS=/screen \
+           -dDownsampleColorImages=true \
+           -dDownsampleGrayImages=true \
+           -dDownsampleMonoImages=true \
+           -dColorImageResolution=96 \
+           -dGrayImageResolution=96 \
+           -dMonoImageResolution=96 \
+           -sDEVICE=pdfwrite -sOutputFile="$(basename "$dir")/$file" \
+           "$dir/$file"
     done
 done
